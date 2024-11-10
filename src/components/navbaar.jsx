4c82cc6e9684f2eb5e3922277ca36fc364ee25ec
shivamwebsite <141153430+shivamwebsite@users.noreply.github.com>
2024-11-10 22:18:24 +0530

    done

---
+++ src/components/navbaar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbaar.css'
+import { RxDragHandleHorizontal } from "react-icons/rx";
+function navbaar() {
+
+  return (
+    <>
+      <div className='bg-neutral-900 text-slate-100 flex justify-between px-10 py-5 text-lg ' id='container'>
+        <div>Task Manager</div>
+        <RxDragHandleHorizontal id='handle'/>
+        <div className='container1 flex gap-10' id='container1'>
+          <div class="change">Home</div>
+          <div>About</div>
+          <div>Contact Us</div>
+        </div>
+        {/* <div className='gap-10' id='container2'>
+          <div>Home</div>
+          <div>About</div>
+          <div>Contact Us</div>
+        </div> */}
+      </div>
+    </>
+  )
+}
+
+
+export default navbaar
